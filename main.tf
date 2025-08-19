dd5e6de5072445c728a4f57bf6c2380590235b1f
ganesh-redy <ganeshreddy7610@gmail.com>
2025-08-19 15:53:33 +0530

    f

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "inst1" {
+  name = "insta"
+  zone = "us-central1-c"
+  boot_disk {
+    initialize_params {
+      image = "centos-stream"
+    }
+  }
+  machine_type = "e2-medium"
+  network_interface {
+    network = "default"
+    access_config {
+      
+    }
+  }
+}
